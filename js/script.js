fc0986d07b057cb039f54376812d4aab5d63ac59
amirtahaafkhami <96968910+amirtahaafkhami@users.noreply.github.com>
2023-05-05 16:14:23 +0330

    second commit

--- js/script.js
+++ js/script.js
@@ -1,7 +1,15 @@
 const hamburger_menu = document.querySelector('.hamburger-menu');
 const container = document.querySelector('.container');
 
-hamburger_menu.addEventListener('click', function(){
+hamburger_menu.addEventListener('click', function () {
     container.classList.toggle('active');
 })
 
+
+const btn = document.querySelector('.close');
+const nav = document.querySelector('nav');
+
+btn.addEventListener('click', () => {
+    btn.classList.toggle('closeBtn');
+    nav.classList.toggle('show')
+})
